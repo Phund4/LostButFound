9da35f920e185e48ae5696bc62fd0b795e85dbb5
(danil) <(abc@gmail.com)>
2023-10-03 22:58:46 +0300

    Initial backend

---
+++ LostButFound.API/Controllers/UserController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace LostButFound.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]/[action]/{id?}")]
+    public class UserController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
