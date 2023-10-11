dc3d7892cd0bb4da4d220fd19e53182cfbf9e892
Phunda <phunda73@gmail.com>
2023-10-11 10:48:14 +0300

    delete api

--- LostButFound.API/DAL/Interfaces/IUserRepository.cs
+++
@@ -1,9 +0,0 @@
-﻿using LostButFound.API.Domian;
-
-namespace LostButFound.API.DAL.Interfaces
-{
-    public interface IUserRepository : IBaseRepository<User>
-    {
-        User GetByName(string name);
-    }
-}
